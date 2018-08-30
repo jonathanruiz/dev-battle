e06e7943b2e1fea78fce6cee101d829b25dea897
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2018-08-29 23:41:38 -0400

    checked to see if it gets anything other than a 200 error (OK).

--- js/app.js
+++ js/app.js
@@ -20,8 +20,23 @@ function getRandomID() {
 function getUser(outputId) {
   let idNumber = getRandomID();
 
-  fetch(`https://api.github.com/user/${idNumber}?client_id=&client_secret=`)
-    .then(res => res.json())
+  // Use this in the commandline to see how many requests I have left over.
+  // curl - H "Authorization: token 7c5cc90e9e7eb48e6b486b7276fb8d5b37a1b553" - X GET https://api.github.com/rate_limit
+
+  fetch(`https://api.github.com/user/${idNumber}`, {
+    headers: {
+      Authorization: "token 7c5cc90e9e7eb48e6b486b7276fb8d5b37a1b553" // This is for the OAuth token
+    }
+  })
+    .then(res => {
+      // Check if the app got the "OK"
+      if (res.status !== 200) {
+        throw new Error("There was an error");
+      } else {
+        return res.json();
+      }
+    })
+    .catch(err => console.log(err))
     .then(data => {
       let output = ``;
       output += `
